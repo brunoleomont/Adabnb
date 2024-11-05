c3e69cb783f8c0ab20db621ededcd5fe4eb79cdb
bruno monteiro <bruno.lmonteiro@totvs.com.br>
2024-11-04 22:06:20 -0300

    Add Angular Material

--- src/app/home-card/home-card.component.ts
+++ src/app/home-card/home-card.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { House } from '../models/house';
 import { RouterLink } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-home-card',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, MatButtonModule],
   templateUrl: './home-card.component.html',
   styleUrl: './home-card.component.css'
 })
